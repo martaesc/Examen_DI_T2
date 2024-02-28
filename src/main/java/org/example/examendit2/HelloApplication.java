41eb691ad74cdccaf95bb05d4cd8ef7282c6712f
martaesc <escorihuela_m246@ieseduardoprimo.es>
2024-02-28 18:12:26 +0100

    commit2

--- src/main/java/org/example/examendit2/HelloApplication.java
+++ src/main/java/org/example/examendit2/HelloApplication.java
@@ -7,7 +7,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Clase principal de la aplicación que extiende de Application.
+ * Esta clase es responsable de cargar la vista inicial de la aplicación y mostrarla en la ventana principal.
+ */
 public class HelloApplication extends Application {
+
+    /**
+     * Método de inicio que se llama después de que se ha inicializado el sistema.
+     * Este método carga la vista de inventario desde un archivo FXML y la muestra en la ventana principal.
+     *
+     * @param stage el escenario principal para esta aplicación, en el que se puede establecer la escena.
+     * @throws IOException si el archivo FXML no se puede cargar.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -17,6 +29,9 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    /**Método principal que lanza la aplicación.
+    Params:
+    args – argumentos de línea de comando.*/
     public static void main(String[] args) {
         launch();
     }
